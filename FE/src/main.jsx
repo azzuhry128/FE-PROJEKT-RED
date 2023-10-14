3ce0b5c4f2670d8f2f92634ce2fa9782aaf1d9a3
azzuhry128 <muhammadazzuhry04@gmail.com>
2023-10-14 19:12:23 +0700

    App
    the center of everything

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -1,8 +1,9 @@
 //this is FE-experimental branch, do your witchcrafts here
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <App/>
   </React.StrictMode>,
 )

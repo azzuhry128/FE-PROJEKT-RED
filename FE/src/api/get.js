74703dfe8f98be8633ffa53916dcebdaba790129
azzuhry128 <muhammadazzuhry04@gmail.com>
2023-10-14 19:08:31 +0700

    get functions
    added functions:
    - getAllUserinfo
    - getAllUserMessages
    - getAllUserContacts
    - userData ( the one which will be called upon first time data request )

---
+++ FE/src/api/get.js
@@ -0,0 +1,34 @@
+import axios from "axios"
+
+const getAllUserInfo = async(url) => {
+  const getEvent = new Event("GET_ALL_USER_INFO")
+  // getAllUserInfoLogger(getEvent)
+
+  const result = await axios.get(url).then((response) => response)
+
+  document.dispatchEvent(getEvent)
+  return result
+}
+
+const getAllUserMessages = async(url) => {
+  const result = await axios.get(url).then((response) => response)
+  return result
+}
+
+const getAllUserContacts = async(url) => {
+  const result =await axios.get(url).then((response) => response)
+  return result
+}
+
+const userData = async(infoURL, messageURL, contactsURL) => {
+
+  const info = await getAllUserInfo(infoURL)
+  const messages = await getAllUserMessages(messageURL)
+  const contacts = await getAllUserContacts(contactsURL)
+
+  const data = {info, messages, contacts}
+
+  return data
+}
+
+export { userData }

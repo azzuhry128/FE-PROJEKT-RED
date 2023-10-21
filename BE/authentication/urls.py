8bf65030d7075819f7ddb189a1c3902ec0842118
Dimas Rizqi <64886534+cynchronos@users.noreply.github.com>
2023-10-21 18:55:37 +0700

    Delete BE directory

--- BE/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Login, Register
-from django.views.decorators.csrf import csrf_exempt
-
-urlPatterns = [
-  path("login/", csrf_exempt(Login.as_view())),
-  path("register/", csrf_exempt(Register.as_view())),
-]

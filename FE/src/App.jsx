23f56594ed38772e138cff8971c54a6978581874
azzuhry128 <muhammadazzuhry04@gmail.com>
2023-10-24 11:55:31 +0700

    App.jsx update
    - black magic line deleted
    - added states for landing, login, register, contacts, settings, profile
    - handlers for landing buttons on landing, login, register
    - render logic for landing, login, and register page

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,8 +1,41 @@
+import { useState } from "react"
+import { LandingPageComponent } from "./components/landingPageComponent"
+import { LoginComponent } from "./components/loginComponent"
+
 function App() {
+  const [showLandingPage, setShowLandingPage] = useState(true)
+  const [showLoginPage, setShowLoginPage] = useState(false)
+  const [showRegisterPage, setShowRegisterPage] = useState(false)
+
+  const [showContacts, setShowContacts] = useState(true)
+  const [showSettings, setShowSettings] = useState(false)
+  const [showProfile, setShowProfile] = useState(false)
+
+  const getStartedClickHandler = () => {
+    setShowLandingPage(false)
+    setShowLoginPage(true)
+  }
+
+  const homeClickHandler = () => {
+    setShowLandingPage(true)
+    setShowLoginPage(false)
+  }
+
+  const registerClickHandler = () => {
+    setShowLoginPage(false)
+    setShowRegisterPage(true)
+  }
+  const loginClickHandler = () => {
+    setShowRegisterPage(false)
+    setShowLoginPage(true)
+  }
 
   return (
     <>
-      <h1>this is FE-experimental branch, you can do your black magic here</h1>
+      {showLandingPage && <LandingPageComponent onGetStartedClick={getStartedClickHandler} />}
+      {showLoginPage && <LoginComponent onHomeClick={homeClickHandler} onRegisterClick={registerClickHandler} />}
+      {showRegisterPage && <RegisterComponent onLoginClick={loginClickHandler} />}
+      {/* {showMainPage && <MainComponent onAuthorized={}/>} */}
     </>
   )
 }

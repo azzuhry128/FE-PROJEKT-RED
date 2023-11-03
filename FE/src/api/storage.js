4d28d8e37373d05373e786b080b5fa7139d38388
azzuhry128 <muhammadazzuhry04@gmail.com>
2023-11-03 16:51:46 +0700

    storage API
    - upload user credentials to storage
    - fetching credentials from storage

--- FE/src/api/storage.js
+++ FE/src/api/storage.js
@@ -1,10 +1,19 @@
-const uploadTokenToStorage = (token) => {
+const uploadToken = (token) => {
   localStorage.setItem("auth-token", token)
 }
 
-const fetchTokenFromStorage = () => {
+const fetchToken = () => {
   const token = localStorage.getItem("auth-token")
   return token
 }
 
-export { uploadTokenToStorage, fetchTokenFromStorage }
+const uploadCredentials = (user) => {
+  localStorage.setItem("user-credentials", user)
+}
+
+const fetchCredentials = () => {
+  const credentials = localStorage.getItem("user-credentials")
+  return credentials
+}
+
+export { uploadToken, uploadCredentials, fetchToken, fetchCredentials }

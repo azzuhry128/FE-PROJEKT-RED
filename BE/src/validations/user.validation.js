45d2296ed1dd5c43c727675cf5d8e236276719e9
Kuro Cybria <rizqikurniawan1863@gmail.com>
2023-12-14 21:31:16 +0700

    fixing naming variables

--- BE/src/validations/user.validation.js
+++ BE/src/validations/user.validation.js
@@ -1,4 +1,5 @@
 const { check, validationResult } = require('express-validator');
+const { v4: uuidv4 } = require('uuid');
 
 async function validateUserInfo(req) {
   try {
@@ -49,66 +50,61 @@ async function validateGetUser(data) {
   }
 }
 
-
 async function validateCreateUser(userInput, userExists) {
   try {
     if (userExists) {
-      const error = { success: false, code: 400, message: "Email was already used" };
+      const error = { success: false, code: 400, message: 'Email was already used' };
       throw error;
     }
 
-  
-    const new_user = {
-      user_id: uuidv4(), 
+    const newUser = {
+      user_id: uuidv4(),
       profile_name: userInput.profile_name,
       email: userInput.email,
       image: userInput.image,
       phone: userInput.phone || null,
-      bio: userInput.bio
+      bio: userInput.bio,
     };
 
-  
-    return new_user;
+    return newUser;
   } catch (error) {
-  
     const errors = {
       success: false,
       code: error.code || 500,
-      message: error.message || "validate user failed"
+      message: error.message || 'validate user failed',
     };
     throw errors;
   }
 }
 
 async function validateEditUser(userInput, userData, userExistsByEmail) {
-try {
-  if (userExistsByEmail) {
-  if (userExistsByEmail.user_id !== userData.user_id) {
-      const error = { success: false, code: 400, message: "Email was already used" };
-      throw error;
+  try {
+    if (userExistsByEmail) {
+      if (userExistsByEmail.user_id !== userData.user_id) {
+        const error = { success: false, code: 400, message: 'Email was already used' };
+        throw error;
+      }
+    }
+
+    const updateUser = {
+      user_id: userData.user_id,
+      profile_name: userInput.profile_name || userData.profile_name,
+      email: userInput.email || userData.email,
+      image: userInput.image || userData.image,
+      phone: userInput.phone || userData.phone || null,
+      bio: userInput.bio,
+    };
+
+    return updateUser;
+  } catch (error) {
+    const errors = {
+      success: false,
+      code: error.code || 500,
+      message: error.message || 'validate user failed',
+    };
+    throw errors;
   }
-  }
-
-  const update_user = {
-  user_id: userData.user_id,
-  profile_name: userInput.profile_name || userData.profile_name,
-  email: userInput.email || userData.email,
-  image: userInput.image || userData.image,
-  phone: userInput.phone || userData.phone || null,
-  bio: userInput.bio
-  };
-
-  return update_user;
-} catch (error) {
-  const errors = {
-  success: false,
-  code: error.code || 500,
-  message: error.message || "validate user failed"
-  };
-  throw errors;
 }
-}
-
 
 module.exports = {
   validateUserInfo,

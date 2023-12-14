f42397a306ecf151467300f4cf32a1cd8d7cb65f
azzuhry128 <muhammadazzuhry04@gmail.com>
2023-12-14 23:30:44 +0700

    deleted

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -7,45 +7,6 @@ import { eventListener } from "./service/eventListener"
 
 console.log("App is called")
 
-// upload local storage
-// window.onload = () => {
-//   uploadMessages(messages)
-//   uploadCredentials(fakeUser)
-//   uploadToken(fakeToken)
-// }
-
-// fetch local storage
-// const fetchedMessages = fetchMessages()
-// const fetchedCredentials = fetchCredentials()
-// const fetchedToken = fetchToken()
-// data arrays
-// const messageArray = []
-// const userDataArray = []
-// const userTokenArray = []
-
-// push fetched data into arrays
-// messageArray.push(fetchedMessages)
-// userDataArray.push(fetchedCredentials)
-// userTokenArray.push(fetchedToken)
-
-// check for array content
-// if (messageArray.length === 0) {
-//   console.log('messageArray is empty')
-// }
-
-// if (userDataArray.length === 0) {
-//   console.log('userDataArray is empty')
-// }
-
-// if (userTokenArray.length === 0) {
-//   console.log('userTokenArray is empty')
-// }
-
-// arrays logger
-// console.log(messageArray[0])
-// console.log(userDataArray[0])
-// console.log(userTokenArray[0])
-
 function App() {
 
   return (

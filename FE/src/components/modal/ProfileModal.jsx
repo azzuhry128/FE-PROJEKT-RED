743e464c07be25fb9639bd4479091f220557525d
Alphaflux_ <ahmad.farhan.nugraha@gmail.com>
2024-01-05 21:41:19 +0700

    ProfileModal.jsx

--- FE/src/components/modal/ProfileModal.jsx
+++ FE/src/components/modal/ProfileModal.jsx
@@ -1,16 +1,24 @@
-import { Box, Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
+import { Box, Button, Modal, ModalBody, ModalContent, Text,ModalFooter, ModalHeader,Avatar,AvatarBadge, ModalOverlay    , useDisclosure, HStack } from "@chakra-ui/react";
 
 function ProfileModal({isOpen, onClose}){
 
     return(
         <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalContent position='absolute' left='0' margin='0.5rem' width='356px' alignSelf='start' containerProps={{ justifyContent: 'flex-end', paddingRight: '2rem' }}>
-                    <ModalHeader>profile</ModalHeader>
+                <ModalContent position='absolute' left='0' margin='0.5rem' width='356px' height='max-content' alignSelf='start' containerProps={{ justifyContent: 'flex-end', paddingRight: '2rem' }}>
+                    <ModalHeader></ModalHeader>
                     <ModalBody width='6rem'>
-                        <Text>hello</Text>
+                        <HStack>
+                            <Box>
+                                <Avatar src='https://bit.ly/broken-link' size='lg'/>
+                            </Box>
+                            <Box marginLeft='0.5rem'>
+                                <Text as='b' marginBottom='5rem' >Name</Text>
+                                <Text>Email</Text>
+                                <Text>Username</Text>
+                            </Box>
+                        </HStack>
                     </ModalBody>
                     <ModalFooter display="flex" gap={4}>
-                        <Button onClick={onClose} colorScheme="blue">close</Button>
                     </ModalFooter>
                 </ModalContent>
         </Modal>

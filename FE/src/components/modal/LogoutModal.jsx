bb73298b146d659fcac743e671b1365c521bd2fd
muhammad rofif azzuhry <muhammadazzuhry04@gmail.com>
2024-05-01 15:53:43 +0700

    LogoutModal logic #1
    + wipe cache
    + redirect user to login

--- FE/src/components/modal/LogoutModal.jsx
+++ FE/src/components/modal/LogoutModal.jsx
@@ -11,8 +11,26 @@ import {
     Center,
     Text,
 } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
 const LogoutModal = (props) => {
+    const navigate = useNavigate()
+    const wipeCache = () => {
+        const keys = ['account', 'contact', 'message']
+        for (const key of keys) {
+            localStorage.removeItem(key)
+        }
+    }
+
+    const redirectToLogin = () => {
+        navigate("/login");
+    }
+
+    const handleClick = () => {
+        wipeCache()
+        redirectToLogin()
+    }
+
     if(props.type != 'LogoutModal') {
         return null
     }
@@ -22,17 +40,17 @@ const LogoutModal = (props) => {
     <Modal isOpen={props.isOpen} onClose={props.onClose} isCentered>
     <ModalOverlay />
     <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader>are you sure?</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-            <Text>are you sure you want to logout?</Text>
+            <Text>your saved data will be deleted and you will have to login next time </Text>
         </ModalBody>
 
         <ModalFooter>
         <Button colorScheme='blue' mr={3} onClick={props.onClose}>
             no
         </Button>
-        <Button colorScheme='red'>yes</Button>
+        <Button onClick={() => handleClick()} colorScheme='red'>yes</Button>
         </ModalFooter>
     </ModalContent>
     </Modal>
